42364c635e37bc188878b94624ab796c012b568f
Jerry <shengfengzhao@icloud.com>
2018-08-12 23:29:55 +0800

    修改Router

--- Node server/Trip/Router.js
+++ Node server/Trip/Router.js
@@ -17,14 +17,13 @@ router.get('/unfinished', function(req, res, next) {
         require('./APIController').getUnfinished(session, (err, ordernumber) => {
             var response = { 
                 code: err ? err.code : 0, 
-                msg : err ? err.msg : ordernumber ? '有可恢复行程' : '无可恢复行程', 
+                msg : err ? err.msg : ordernumber ? '有可恢复的行程' : '无可恢复的行程', 
                 ordernumber: ordernumber 
             };
             res.send(JSON.stringify(response));
         });
     } else {
-        var response = { code: -1, msg : '参数不正确' }
-        res.send(JSON.stringify(response));
+        res.send(JSON.stringify({ code: -1, msg : '参数不正确' }));
     }
 });
 
@@ -39,57 +38,14 @@ router.get('/create', function(req, res, next) {
             res.send(JSON.stringify(response));
         });
     } else {
-        var response = { code: -1, msg : '参数不正确' }
-        res.send(JSON.stringify(response));
-    }
-});
-
-/**
- * 取消行程(开始前)
- */
-router.get('/cancel', function(req, res, next) {
-    var session = req.query.session;
-    var sessionCheck = session != null && session.length > 0;
-
-    var ordernumber = req.query.ordernumber;
-    var ordernumberCheck = ordernumber != null && ordernumber.length > 0;
-
-    if (sessionCheck && ordernumberCheck) {
-        require('./APIController').cancel(session, ordernumber, (err) => {
-            var response = { code: err ? err.code : 0, msg : err ? err.msg : '行程已取消' };
-            res.send(JSON.stringify(response));
-        });
-    } else {
-        var response = { code: -1, msg : '参数不正确' }
-        res.send(JSON.stringify(response));
-    }
-});
-
-/**
- * 开始行程
- */
-router.get('/start', function(req, res, next) {
-    var session = req.query.session;
-    var sessionCheck = session != null && session.length > 0;
-
-    var ordernumber = req.query.ordernumber;
-    var ordernumberCheck = ordernumber != null && ordernumber.length > 0;
-
-    if (sessionCheck && ordernumberCheck) {
-        require('./APIController').start(session, ordernumber, (err) => {
-            var response = { code: err ? err.code : 0, msg : err ? err.msg : '行程已开始' };
-            res.send(JSON.stringify(response));
-        });
-    } else {
-        var response = { code: -1, msg : '参数不正确' }
-        res.send(JSON.stringify(response));
+        res.send(JSON.stringify({ code: -1, msg : '参数不正确' }));
     }
 });
 
 /**
  * 结束行程
  */
-router.get('/stop', function(req, res, next) {
+router.get('/end', function(req, res, next) {
     var session = req.query.session;
     var sessionCheck = session != null && session.length > 0;
 
@@ -97,20 +53,22 @@ router.get('/stop', function(req, res, next) {
     var ordernumberCheck = ordernumber != null && ordernumber.length > 0;
 
     if (sessionCheck && ordernumberCheck) {
-        require('./APIController').stop(session, ordernumber, (err) => {
-            var response = { code: err ? err.code : 0, msg : err ? err.msg : '行程已结束' };
-            res.send(JSON.stringify(response));
+        require('./APIController').end(session, ordernumber, (err) => {
+            if (err) {
+                res.send(JSON.stringify({code: err.code, msg: err.msg}));
+            } else {
+                res.send(JSON.stringify({code: 0, msg: '行程已结束'}))
+            }
         });
     } else {
-        var response = { code: -1, msg : '参数不正确' }
-        res.send(JSON.stringify(response));
+        res.send(JSON.stringify({ code: -1, msg : '参数不正确' }));
     }
 });
 
 /**
  * 发出求救
  */
-router.get('/sos/start', function(req, res, next) {
+router.get('/sos', function(req, res, next) {
     var session = req.query.session;
     var sessionCheck = session != null && session.length > 0;
 
@@ -118,41 +76,22 @@ router.get('/sos/start', function(req, res, next) {
     var ordernumberCheck = ordernumber != null && ordernumber.length > 0;
 
     if (sessionCheck && ordernumberCheck) {
-        require('./APIController').startSOS(session, ordernumber, (err) => {
-            var response = { code: err ? err.code : 0, msg : err ? err.msg : '已发出求救' };
-            res.send(JSON.stringify(response));
+        require('./APIController').SOS(session, ordernumber, (err) => {
+            if (err) {
+                res.send(JSON.stringify({code: err.code, msg: err.msg}));
+            } else {
+                res.send(JSON.stringify({code: 0, msg: '已发出求救'}));
+            }
         });
     } else {
-        var response = { code: -1, msg : '参数不正确' }
-        res.send(JSON.stringify(response));
-    }
-});
-
-/**
- * 解除求救
- */
-router.get('/sos/stop', function(req, res, next) {
-    var session = req.query.session;
-    var sessionCheck = session != null && session.length > 0;
-
-    var ordernumber = req.query.ordernumber;
-    var ordernumberCheck = ordernumber != null && ordernumber.length > 0;
-
-    if (sessionCheck && ordernumberCheck) {
-        require('./APIController').stopSOS(session, ordernumber, (err) => {
-            var response = { code: err ? err.code : 0, msg : err ? err.msg : '已解除求救' };
-            res.send(JSON.stringify(response));
-        });
-    } else {
-        var response = { code: -1, msg : '参数不正确' }
-        res.send(JSON.stringify(response));
+        res.send(JSON.stringify({ code: -1, msg : '参数不正确' }));
     }
 });
 
 /**
  * 获取行程信息
  */
-router.get('/info/get', function(req, res, next) {
+router.get('/info', function(req, res, next) {
     var session = req.query.session;
     var sessionCheck = session != null && session.length > 0;
 
@@ -168,43 +107,22 @@ router.get('/info/get', function(req, res, next) {
             };
             res.send(JSON.stringify(response));
         });
-    } else {
-        var response = { code: -1, msg : '参数不正确' }
-        res.send(JSON.stringify(response));
-    }
-});
-
-/**
- * 更新行程信息
- */
-router.get('/info/update', function(req, res, next) {
-    var session = req.query.session;
-    var sessionCheck = session != null && session.length > 0;
-
-    var ordernumber = req.query.ordernumber;
-    var ordernumberCheck = ordernumber != null && ordernumber.length > 0;
-
-    var destination = req.query.destination;
-    var destinationCheck = destination != null && destination.length > 0 && destination.length <= 32;
-
-    var tool = req.query.tool;
-    var toolCheck = tool != null && tool.length > 0 && tool.length <= 32;
-
-    if (sessionCheck && ordernumberCheck && destinationCheck && toolCheck) {
-        require('./APIController').updateTripInfo(session, ordernumber, destination, tool, (err) => {
-            var response = { code: err ? err.code : 0, msg : err ? err.msg : '更新成功' };
-            res.send(JSON.stringify(response));
+        require('./APIController').getTripInfo(session, ordernumber, (err, result) => {
+            if (err) {
+                res.send(JSON.stringify({code: err.code, msg: err.msg}));
+            } else {
+                res.send(JSON.stringify({code: 0, msg : '查询成功', info: JSON.parse(result)}));
+            }
         });
     } else {
-        var response = { code: -1, msg : '参数不正确' }
-        res.send(JSON.stringify(response));
+        res.send(JSON.stringify({ code: -1, msg : '参数不正确' }));
     }
 });
 
 /**
- * 获取位置(轨迹)
+ * 获取行程路线
  */
-router.get('/location/all', function(req, res, next) {
+router.get('/polyline', function(req, res, next) {
     var session = req.query.session;
     var sessionCheck = session != null && session.length > 0;
 
@@ -212,23 +130,39 @@ router.get('/location/all', function(req, res, next) {
     var ordernumberCheck = ordernumber != null && ordernumber.length > 0;
 
     if (sessionCheck && ordernumberCheck) {
-        require('./APIController').getTripLocationLocus(session, ordernumber, (err, result) => {
-            var response = { 
-                code: err ? err.code : 0, 
-                msg : err ? err.msg : '查询成功', 
-                result: err ? null : JSON.parse(result)
-            };
-            res.send(JSON.stringify(response));
+        require('./APIController').getTripPolyline(session, ordernumber, (err, result) => {
+            if (err) {
+                res.send(JSON.stringify({code: err.code, msg: err.msg, polyline: null}));
+            } else {
+                res.send(JSON.stringify({code: 0, msg: '行程路线获取成功', polyline: JSON.parse(result)}));
+            }
         });
     } else {
-        var response = { code: -1, msg : '参数不正确' }
-        res.send(JSON.stringify(response));
+        res.send(JSON.stringify({ code: -1, msg : '参数不正确' }));
     }
 });
 
 /**
- * 上报位置(应该用不到,作废?)
+ * 获取行程日志
  */
-// router.get('/location/upload', function(req, res, next) {});
+router.get('/logs', function(req, res, next) {
+    var session = req.query.session;
+    var sessionCheck = session != null && session.length > 0;
+
+    var ordernumber = req.query.ordernumber;
+    var ordernumberCheck = ordernumber != null && ordernumber.length > 0;
+
+    if (sessionCheck && ordernumberCheck) {
+        require('./APIController').getTripLogs(session, ordernumber, (err, result) => {
+            if (err) {
+                res.send(JSON.stringify({code: err.code, msg: err.msg, polyline: null}));
+            } else {
+                res.send(JSON.stringify({code: 0, msg: '行程日志获取成功', polyline: JSON.parse(result)}));
+            }
+        });
+    } else {
+        res.send(JSON.stringify({ code: -1, msg : '参数不正确' }));
+    }
+});
 
 module.exports = router;
