069f183f873a7b28b09a5099966edc51b726e7db
Jerry <shengfengzhao@icloud.com>
2018-12-06 20:42:12 +0800

    初始化Beego api项目

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	_ "An_Server/routers"
+
+	"github.com/astaxie/beego"
+)
+
+func main() {
+	if beego.BConfig.RunMode == "dev" {
+		beego.BConfig.WebConfig.DirectoryIndex = true
+		beego.BConfig.WebConfig.StaticDir["/swagger"] = "swagger"
+	}
+	beego.Run()
+}
