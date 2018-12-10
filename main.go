772f8f8013e688d014d0099edbd3e6763ffeaf33
Jerry <shengfengzhao@icloud.com>
2018-12-10 20:53:58 +0800

    初始化 bee web项目

--- main.go
+++ main.go
@@ -1,15 +1,11 @@
 package main
 
 import (
-	_ "An_Server/routers"
-
+	_ "An_Server/An_Server/routers"
 	"github.com/astaxie/beego"
 )
 
 func main() {
-	if beego.BConfig.RunMode == "dev" {
-		beego.BConfig.WebConfig.DirectoryIndex = true
-		beego.BConfig.WebConfig.StaticDir["/swagger"] = "swagger"
-	}
 	beego.Run()
 }
+
